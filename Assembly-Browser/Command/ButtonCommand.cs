ff28dd7e2a66dc2336cff3d8ee782f1d90c64c59
Vladimir <lililolo1222@mail.ru>
2021-11-30 21:12:07 +0300

    ButtonCommand

---
+++ Assembly-Browser/Command/ButtonCommand.cs
@@ -0,0 +1,32 @@
+﻿using Assembly_Browser.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ButtonViewModel = Assembly_Browser.ViewModel.ButtonViewModel;
+
+namespace Assembly_Browser
+{
+    class ButtonCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        ButtonViewModel _buttonViewModel;
+
+        public ButtonCommand(ButtonViewModel viewModel)
+        {
+            _buttonViewModel = viewModel;
+        }
+        //может ли выполняться
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        //само выполнение команды
+        public void Execute(object parameter)
+        {
+            _buttonViewModel.OnExecute();
+        }
+    }
+}
