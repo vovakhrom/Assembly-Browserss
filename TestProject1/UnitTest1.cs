9e99a24631c29cfcbaac05f8048b37236c6f5216
Vladimir <lililolo1222@mail.ru>
2021-11-30 21:13:27 +0300

    TestProject1

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using AssemblyAnalyzer;
+using System.Collections.Generic;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        AssemblyBrowser assemblyBrowser = new AssemblyBrowser();
+        private string path = "C:\\Users\\37529\\source\\repos\\Assembly-Browser\\ProjectForUnitTest\\bin\\Debug\\ProjectForUnitTest.dll";
+        List<Container> assemblyInfo;
+
+        [SetUp]
+        public void SetUp()
+        {
+            assemblyInfo = assemblyBrowser.GetAssemblyInfo(path);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(assemblyInfo == null, false, "Couldn't load assembly info.");
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(assemblyInfo[0].Class, "private   class  Program", "Wrong name for first class.");
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(assemblyInfo[0].Signature, "ProjectForUnitTest", "Wrong first namespace.");
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Assert.AreEqual(assemblyInfo.Count, 2, "Wrong number of namespaces.");
+        }
+
+        [Test]
+        public void Test5()
+        {
+            Assert.AreEqual(assemblyInfo[0].Members.Count, 1, "Wrong number of members in first namespace.");
+        }
+    }
+}
