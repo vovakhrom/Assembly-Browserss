75b270c464d9a69e392f881472f31ff1ff05b544
Vladimir <lililolo1222@mail.ru>
2021-11-30 21:11:50 +0300

    Container

---
+++ Assembly-Analyzer/Container.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyAnalyzer
+{
+    public class Container : MemberInfo
+    {
+        public List<MemberInfo> Members { get; set; }
+
+        public Container(string @namespace, string @class, string signature, List<MemberInfo> members) : base(@namespace, @class)
+        {
+            Signature = signature;
+            Members = members;
+        }
+        public Container(string @namespace, string @class) : base(@namespace, @class)
+        {
+            Members = new List<MemberInfo>();
+        }
+
+    }
+}
