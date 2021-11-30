270119cbb775d3d01f59c269f409a049e3f4c948
Vladimir <lililolo1222@mail.ru>
2021-11-30 21:15:25 +0300

    other files for assembly-analyzer

---
+++ Assembly-Analyzer/MemberInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyAnalyzer
+{
+    public class MemberInfo
+    {
+        public string Signature { get; set; }
+        public string Class { get; set; }
+
+        public MemberInfo(string signature, string @class)
+        {
+            Signature = signature;
+            Class = @class;
+        }
+    }
+}
---
+++ Assembly-Analyzer/NamespaceInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyAnalyzer
+{
+    public class NamespaceInfo
+    {
+        public string Signature { get; set; }
+        public List<Container> MemberInfo { get; set; }
+
+        public NamespaceInfo(string signature)
+        {
+            MemberInfo = new List<Container>();
+            Signature = signature;
+        }
+
+        public NamespaceInfo(List<Container> memberInfo, string signature)
+        {
+            Signature = signature;
+            MemberInfo = memberInfo;
+        }
+    }
+}
